6678c964e860b019b1a91b17a2a56b356e8ae589
Mike Lee <mikemunkyu.lee@gmail.com>
2023-05-10 21:29:28 -0400

    Add notification logic

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ import time
 from scouters import AirForceOneScouter
 
 
+from plyer import notification
+
 RETRY_IN_SECONDS = 10
 
 
@@ -16,9 +18,21 @@ while True:
     size_found = airforce_scouter.check_footlocker()
     airforce_scouter.report_search_result(size_found)
 
+    notification.notify(
+        title="Found size",
+        message="Found a size at Footlocker!",
+        app_name="Scouter",
+    )
+
     print("Checking @ Nike...")
     size_found = airforce_scouter.check_nike()
     airforce_scouter.report_search_result(size_found)
+
+    notification.notify(
+        title="Found size",
+        message="Found a size at Nike!",
+        app_name="Scouter",
+    )
 
     print(f"Checking again in {RETRY_IN_SECONDS} seconds...")
     time.sleep(RETRY_IN_SECONDS)
